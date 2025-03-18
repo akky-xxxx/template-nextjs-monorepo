e39dcf44644947532eed834b9c5e78be107da81a
akky-xxxx <114.s.aki@gmail.com>
2025-03-18 22:08:42 +0900

    feat(client-screen): implement pageComponents/Home as temporary

---
+++ applications/client-screen/src/pageComponents/Home/index.tsx
@@ -0,0 +1,8 @@
+import {SampleServerComponent} from "@/serverComponents/SampleServerComponent"
+
+export const Home = () => (
+  <>
+    <h1>Home as temporary</h1>
+    <SampleServerComponent />
+  </>
+)
