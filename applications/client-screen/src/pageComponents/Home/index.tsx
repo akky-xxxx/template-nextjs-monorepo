2f85ba81352b87b62815871361771d13450100d0
akky-xxxx <114.s.aki@gmail.com>
2025-03-18 22:12:07 +0900

    style(client-screen): apply fix task

--- applications/client-screen/src/pageComponents/Home/index.tsx
+++ applications/client-screen/src/pageComponents/Home/index.tsx
@@ -1,8 +1,8 @@
-import {SampleServerComponent} from "@/serverComponents/SampleServerComponent"
+import { SampleServerComponent } from "@/serverComponents/SampleServerComponent"
 
 export const Home = () => (
-  <>
+  <React.Fragment>
     <h1>Home as temporary</h1>
     <SampleServerComponent />
-  </>
+  </React.Fragment>
 )
