7dbd93d5ed01d9f122cd523686beb9fc921573f3
akky-xxxx <114.s.aki@gmail.com>
2025-03-20 00:48:15 +0900

    feat(client-screen): implement /api/routeHandler-even-odd for sample

---
+++ applications/client-screen/src/app/api/routeHandler-even-odd/route.ts
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import type { NextRequest } from "next/server"
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams: requestedSearchParameters } = request.nextUrl
+  const numberQueryValue = requestedSearchParameters.get("numberFromFront")
+  const requestSearchParameters = new URLSearchParams()
+  if (numberQueryValue !== null && numberQueryValue !== "") requestSearchParameters.set("numberFromBff", numberQueryValue)
+
+  const requestUrl = `http://localhost:3010/api/be-even-odd?${requestSearchParameters.toString()}`
+  const result = await fetch(requestUrl)
+    .then((response) => response.json())
+    // TODO: api 周りの型を整備したら不要になる
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
+    .then((data) => data.result)
+    .catch((error: unknown) => {
+      // eslint-disable-next-line no-console
+      console.error(error)
+      return "error"
+    })
+  return Response.json(result)
+}
