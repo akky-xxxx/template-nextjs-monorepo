f2db03f3f63ce2aea1e8316b1e126fa327d21fd5
akky-xxxx <114.s.aki@gmail.com>
2025-03-20 00:49:35 +0900

    feat(client-screen): use ClientFetch in pageComponents/Home

--- applications/client-screen/src/pageComponents/Home/index.tsx
+++ applications/client-screen/src/pageComponents/Home/index.tsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react"
 
+import { ClientFetch } from "@/clientComponents/ClientFetch"
 import { SampleServerComponent } from "@/serverComponents/SampleServerComponent"
 
 export const Home = () => (
   <Fragment>
     <h1>Home as temporary</h1>
     <SampleServerComponent />
+    <ClientFetch />
   </Fragment>
 )
