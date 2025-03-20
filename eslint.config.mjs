961ab8f5002ecafcf8f5d8f77000ba2bf02354ab
akky-xxxx <114.s.aki@gmail.com>
2025-03-20 21:40:06 +0900

    chore(root): use IGNORE_FOR_ROOT at eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import eslintConfigSCAll from "eslint-config-sc-all"
 
 import { CONFIG } from "./config/eslint/constants/CONFIG/index.mjs"
+import { IGNORE_FOR_ROOT } from "./config/eslint/constants/IGNORE_FOR_ROOT/index.mjs"
 
 export default [
   eslintConfigSCAll.getConfigs("javascript"),
   CONFIG,
+  IGNORE_FOR_ROOT,
 ].flat()
