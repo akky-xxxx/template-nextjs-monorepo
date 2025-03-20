6b6a6c332108fe52bd8aac1e6125cceeb19e5064
akky-xxxx <114.s.aki@gmail.com>
2025-03-20 15:13:57 +0900

    refactor(client-screen): replace to file that made by openapi

--- applications/client-screen/spec/mock/handlersForRouteHandler/handlers/routeHandler-even-odd/get/index.ts
+++ applications/client-screen/spec/mock/handlersForRouteHandler/handlers/routeHandler-even-odd/get/index.ts
@@ -3,6 +3,8 @@ import { http, HttpResponse } from "msw"
 import { logger } from "../../../../shared/modules/logger"
 import { PREFIX } from "../../../constants/PREFIX"
 
+import type { GetApiRouteHandlerEvenOdd200, GetApiRouteHandlerEvenOddParams } from "@/libs/model/client-routeHandler"
+
 const MOCK_TARGET = `${PREFIX}/routeHandler-even-odd`
 const METHOD = "get"
 
@@ -16,17 +18,20 @@ export const routeHandlerEvenOddGet = http[METHOD](MOCK_TARGET, ({ request }) =>
     action: "request",
   })
   try {
-    const numberQueryValue = new URL(request.url).searchParams.get("numberFromFront")
+    const numberQueryValue = new URL(request.url).searchParams.get(
+      "numberFromFront" satisfies keyof GetApiRouteHandlerEvenOddParams,
+    )
 
     if (numberQueryValue === null || !(/^\d+$/.test(numberQueryValue))) {
       outputLog({
         action: "bad request",
         data: { numberQueryValue },
       })
-      return new HttpResponse("NaN")
+      const responseValue: GetApiRouteHandlerEvenOdd200 = "NaN"
+      return new HttpResponse(responseValue)
     }
 
-    const result = Number(numberQueryValue) % 2 === 0 ? "even" : "odd"
+    const result: GetApiRouteHandlerEvenOdd200 = Number(numberQueryValue) % 2 === 0 ? "even" : "odd"
     outputLog({
       action: "response",
       data: result,
--- applications/client-screen/src/clientComponents/ClientFetch/modules/useClientFetch/index.ts
+++ applications/client-screen/src/clientComponents/ClientFetch/modules/useClientFetch/index.ts
@@ -1,5 +1,7 @@
 import { useEffect, useRef, useState } from "react"
 
+import { getApiRouteHandlerEvenOdd } from "@/libs/apiClient/client-routeHandler"
+
 export const useClientFetch = () => {
   const inputRef = useRef<HTMLInputElement>(null)
   const [resultText, setResultText] = useState("")
@@ -10,18 +12,11 @@ export const useClientFetch = () => {
 
   const handleClickButton = () => {
     if (!inputRef.current || inputRef.current.value === "") return
-    const url = new URLSearchParams()
-    url.set("numberFromFront", inputRef.current.value)
-    const requestUri = `/api/routeHandler-even-odd?${url.toString()}`
-    fetch(requestUri)
-      .then((res) => res.json())
-      .then((data) => {
-        // TODO: api 周りの型を整備したら不要になる
-        if (typeof data !== "string") return
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    getApiRouteHandlerEvenOdd({ numberFromFront: inputRef.current.value })
+      .then((response) => {
+        const { data } = response
         setResultText(data)
-      })
-      .catch(() => {
-        setResultText("error!")
       })
   }
 
