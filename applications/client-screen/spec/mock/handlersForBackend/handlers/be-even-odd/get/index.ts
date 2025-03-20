cb1319d3f3538086854a9318101735796b3b31e5
akky-xxxx <114.s.aki@gmail.com>
2025-03-20 17:42:34 +0900

    docs(client-screen): change type checling to strict at beEvenOddGet

--- applications/client-screen/spec/mock/handlersForBackend/handlers/be-even-odd/get/index.ts
+++ applications/client-screen/spec/mock/handlersForBackend/handlers/be-even-odd/get/index.ts
@@ -3,17 +3,26 @@ import { http, HttpResponse } from "msw"
 import { logger } from "../../../../shared/modules/logger"
 import { HOST_PREFIX } from "../../../constants/HOST_PREFIX"
 
+import type { WithoutPathParameter } from "../../../../shared/types/WithoutPathParameter"
+import type { WithoutRequestBody } from "../../../../shared/types/WithoutRequestBody"
 import type { getGetApiBeEvenOddUrl } from "@/libs/apiClient/client-backend"
 import type { GetApiBeEvenOdd200, GetApiBeEvenOddParams } from "@/libs/model/client-backend"
 
-const MOCK_TARGET = `${HOST_PREFIX}/be-even-odd` satisfies ReturnType<typeof getGetApiBeEvenOddUrl>
+type Endpoint = ReturnType<typeof getGetApiBeEvenOddUrl>
+const MOCK_TARGET = `${HOST_PREFIX}/be-even-odd` satisfies Endpoint
 const METHOD = "get"
 
 const outputLog = logger({
   method: METHOD,
   mockTarget: MOCK_TARGET,
 })
-export const beEvenOddGet = http[METHOD](MOCK_TARGET, ({ request }) => {
+
+export const beEvenOddGet = http[METHOD]<
+  WithoutPathParameter,
+  WithoutRequestBody,
+  GetApiBeEvenOdd200,
+  Endpoint
+>(MOCK_TARGET, ({ request }) => {
   outputLog({
     action: "request",
   })
@@ -24,7 +33,7 @@ export const beEvenOddGet = http[METHOD](MOCK_TARGET, ({ request }) => {
         action: "response",
         data: { numberQueryValue },
       })
-      return HttpResponse.json("NaN" satisfies GetApiBeEvenOdd200)
+      return HttpResponse.json("NaN")
     }
     const result: GetApiBeEvenOdd200 = Number(numberQueryValue) % 2 === 0 ? "even" : "odd"
     outputLog({
