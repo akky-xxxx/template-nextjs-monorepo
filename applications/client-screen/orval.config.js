04904ae1d1280b24eccf081213ee794a28bf45df
akky-xxxx <114.s.aki@gmail.com>
2025-03-20 16:35:40 +0900

    chore(client-screen): add config for client-backend into orval

--- applications/client-screen/orval.config.js
+++ applications/client-screen/orval.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  "client-backend": {
+    input: "../../documents/openapi/dist/client-backend.yaml",
+    output: {
+      baseUrl: "http://localhost:3100/",
+      client: "fetch",
+      schemas: "./src/libs/model/client-backend",
+      target: "./src/libs/apiClient/client-backend.ts",
+    },
+  },
   "client-routeHandler": {
     input: "../../documents/openapi/dist/client-routeHandler.yaml",
     output: {
