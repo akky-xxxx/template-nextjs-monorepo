e3477dc9f2fb3c301efed18cde4acbd4370181a1
akky-xxxx <114.s.aki@gmail.com>
2025-03-22 20:26:44 +0900

    test(client-backend): add test case into GET: /api/even-odd

--- applications/client-backend/spec/tests/apiEvenOddGet/index.test.ts
+++ applications/client-backend/spec/tests/apiEvenOddGet/index.test.ts
@@ -9,6 +9,22 @@ describe("apiEvenOddGet", () => {
     await server.close()
   })
 
+  it('numberFromRouteHandler が 0 でリクエストされたら { judgementResult: "even" } を返す', async () => {
+    const response = await server.inject({
+      method: "get",
+      url: "/api/be-even-odd",
+      query: {
+        numberFromRouteHandler: "0",
+      },
+    })
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toStrictEqual({
+      responseData: {
+        judgementResult: "even",
+      },
+    })
+  })
+
   it('numberFromRouteHandler が 1 でリクエストされたら { judgementResult: "odd" } を返す', async () => {
     const response = await server.inject({
       method: "get",
@@ -23,5 +39,30 @@ describe("apiEvenOddGet", () => {
         judgementResult: "odd",
       },
     })
+  })
+
+  it('numberFromRouteHandler が数字以外でリクエストされたら { judgementResult: "NaN" } を返す', async () => {
+    const response = await server.inject({
+      method: "get",
+      url: "/api/be-even-odd",
+      query: {
+        numberFromRouteHandler: "not a number",
+      },
+    })
+    expect(response.statusCode).toBe(200)
+    expect(response.json()).toStrictEqual({
+      responseData: {
+        judgementResult: "NaN",
+      },
+    })
+  })
+
+  it('numberFromRouteHandler が未指定でリクエストされたら status code 400 を返す', async () => {
+    const response = await server.inject({
+      method: "get",
+      url: "/api/be-even-odd",
+    })
+    expect(response.statusCode).toBe(400)
+    expect(response.statusMessage).toBe("Bad Request")
   })
 })
